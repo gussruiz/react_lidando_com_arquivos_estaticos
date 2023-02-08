9d071da88b89c437b6919f07fe9c910e5a47cf5d
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-08 20:47:36 -0300

    svg as component and reset.css

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,13 @@
 import styles from "./Cardapio.module.scss";
+import {ReactComponent as Logo} from '../../assets/logo.svg' 
 
 export default function Cardapio() {
   return (
     <main>
-      <nav className="menu">
-        <p>oi</p>
+      <nav>
+        <nav className={styles.menu}>
+          <Logo/>
+        </nav>
       </nav>
     </main>
   )
