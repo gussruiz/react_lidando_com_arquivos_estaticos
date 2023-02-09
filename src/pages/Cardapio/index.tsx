acdc929578626359e91746a7159940886d1cf837
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-08 21:28:53 -0300

    header and search bar all done, no logic

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,14 +1,31 @@
 import styles from "./Cardapio.module.scss";
-import {ReactComponent as Logo} from '../../assets/logo.svg' 
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+import Buscador from "./Buscador";
+import { useState } from "react";
 
 export default function Cardapio() {
-  return (
-    <main>
-      <nav>
-        <nav className={styles.menu}>
-          <Logo/>
-        </nav>
-      </nav>
-    </main>
-  )
+
+	const [busca, setBusca] = useState('');
+
+	return (
+		<main>
+			<nav>
+				<nav className={styles.menu}>
+					<Logo />
+					<header className={styles.header}>
+						<div className={styles.header__text}>
+							A casa do código e da massa
+						</div>
+					</header>
+					<section className={styles.cardapio}>
+						<h3 className={styles.cardapio__titulo}>Cardápio</h3>
+						<Buscador
+							busca={busca}
+							setBusca={setBusca}
+						/>
+					</section>
+				</nav>
+			</nav>
+		</main>
+	)
 }
